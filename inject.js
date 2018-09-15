9a3f17fe9354fa78b567f85f61b4333ab08a7917
arpruss <arpruss@gmail.com>
2018-09-15 08:00:14 -0500

    works

--- inject.js
+++ inject.js
@@ -1,5 +1,6 @@
+// based on https://gist.github.com/nok/a98c7c5ce0d0803b42da50c4b901ef84    
 var body = document.getElementsByTagName('body')[0];
 var script = document.createElement('script');
 script.setAttribute('type', 'text/javascript');
-script.setAttribute('src', 'content.js');
+script.setAttribute('src', chrome.extension.getURL('content.js'));
 body.appendChild(script);
